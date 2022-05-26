e46483e100b898ba17f3d2debc6d59f040eadae9
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-26 12:57:40 -0400

    repeat each time user makes a guess

--- main.py
+++ main.py
@@ -14,19 +14,20 @@ turtle.shape(image)
 
 data = pandas.read_csv("./50_states.csv")
 all_states = data.state.to_list()
+guessed_states = []
 
-
-answer_state = screen.textinput(title="Guess the State", prompt = "What's another state's name?")
-print(answer_state)
-
-
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(answer_state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title="Guess the State", prompt = "What's another state's name?")
+ 
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        # t.write(answer_state)
+        t.write(state_data.state.item())
 
 
 turtle.mainloop()
