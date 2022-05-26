4d0848c601276a10f87f8f3c403bd69596d12ca3
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-26 13:01:36 -0400

    change input to title case

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ all_states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt = "What's another state's name?")
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt = "What's another state's name?").title()
  
     if answer_state in all_states:
         guessed_states.append(answer_state)
