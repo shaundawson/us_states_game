12f5c6ee119f872d29eba0a736e9fa589f13585a
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-26 13:49:20 -0400

    generate a new csv of states that have not been guessed on exit

--- main.py
+++ main.py
@@ -16,11 +16,22 @@ data = pandas.read_csv("./50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
 
+
+
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt = "What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+    prompt= "What's another state's name?").title()
 
     if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                 missing_states.append(state)
+                 # print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
+
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -30,6 +41,7 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         # t.write(answer_state)
         t.write(state_data.state.item())
+
 
 
 # turtle.mainloop()
